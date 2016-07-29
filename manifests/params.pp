06c0c614faab2a9bebf06459510add26de230f47
Steve Hall <steve.hall@alchemetrics.co.uk>
2016-07-29 09:24:29 +0100

    Control the Direct Routing settings on the Loadbalanced clients.

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class keepalived::params {
 
   $service = 'keepalived'
   $confdir = '/etc/keepalived'
-  $service_restart = "/sbin/service ${service} reload"
+
 
   # We can't use osfamily since Gentoo's is 'Linux'
   case $::operatingsystem {
@@ -14,21 +14,31 @@ class keepalived::params {
       $package           = 'sys-cluster/keepalived'
       $sysconfdir        = 'conf.d'
       $service_hasstatus = true
+      $service_restart = "/sbin/service ${service} reload"
     }
     'RedHat','Fedora','CentOS','Scientific','Amazon': {
       $package           = 'keepalived'
       $sysconfdir        = 'sysconfig'
       $service_hasstatus = true
+      $service_restart = "/sbin/service ${service} reload"
     }
+
     'Debian', 'Ubuntu': {
       $package           = 'keepalived'
       $sysconfdir        = undef
-      $service_hasstatus = false
+      if ( $::operatingsystem  == 'Ubuntu' and versioncmp($::operatingsystemrelease,'16.04') >= 0 ) {
+        $service_restart = "/bin/systemctl reload keepalived"
+        $service_hasstatus = true
+      } else {
+        $service_restart = "/sbin/service ${service} reload"
+        $service_hasstatus = false
+      }
     }
     default: {
       $package           = 'keepalived'
       $sysconfdir        = undef
       $service_hasstatus = true
+      $service_restart = "/sbin/service ${service} reload"
     }
   }
 
